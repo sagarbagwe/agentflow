059b1a0b4a96b7686b29535fdb3360e05df82543
Sagar Bagwe <90025855+sagarbagwe@users.noreply.github.com>
2026-09-19 16:31:35 +0530

    feat: add real-time execution event broker

---
+++ internal/realtime/broker.go
@@ -0,0 +1,52 @@
+package realtime
+
+import (
+	"context"
+	"sync"
+
+	"github.com/sagarbagwe/agentflow/internal/workflow"
+)
+
+type Broker struct {
+	mu          sync.RWMutex
+	subscribers map[string]map[chan workflow.Event]struct{}
+}
+
+func NewBroker() *Broker {
+	return &Broker{subscribers: make(map[string]map[chan workflow.Event]struct{})}
+}
+
+func (b *Broker) Publish(_ context.Context, event workflow.Event) error {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+	for subscriber := range b.subscribers[event.ExecutionID] {
+		select {
+		case subscriber <- event:
+		default:
+		}
+	}
+	return nil
+}
+
+func (b *Broker) Subscribe(executionID string) (<-chan workflow.Event, func()) {
+	channel := make(chan workflow.Event, 32)
+	b.mu.Lock()
+	if b.subscribers[executionID] == nil {
+		b.subscribers[executionID] = make(map[chan workflow.Event]struct{})
+	}
+	b.subscribers[executionID][channel] = struct{}{}
+	b.mu.Unlock()
+
+	unsubscribe := func() {
+		b.mu.Lock()
+		defer b.mu.Unlock()
+		if subscribers := b.subscribers[executionID]; subscribers != nil {
+			delete(subscribers, channel)
+			close(channel)
+			if len(subscribers) == 0 {
+				delete(b.subscribers, executionID)
+			}
+		}
+	}
+	return channel, unsubscribe
+}
